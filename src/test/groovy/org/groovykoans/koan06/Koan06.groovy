a62fa5e9845f0810587063c73b65628a680311e0
luizhenrique <luizhenrique321@gmail.com>
2016-04-05 10:26:25 -0300

    Realização do exercício 6.

--- src/test/groovy/org/groovykoans/koan06/Koan06.groovy
+++ src/test/groovy/org/groovykoans/koan06/Koan06.groovy
@@ -7,6 +7,8 @@
 
 package org.groovykoans.koan06
 
+import groovy.io.FileType
+
 /**
  * Koan06 - More closures
  *
@@ -19,68 +21,82 @@ package org.groovykoans.koan06
  */
 class Koan06 extends GroovyTestCase {
 
-    void test01_WithMethod() {
-        // The 'with()' method [ http://docs.groovy-lang.org/latest/html/groovy-jdk/java/lang/Object.html#with(groovy.lang.Closure) ]
-        // allows you to access an object within a closure without explicitly referring to it.
+	void test01_WithMethod() {
+		// The 'with()' method [ http://docs.groovy-lang.org/latest/html/groovy-jdk/java/lang/Object.html#with(groovy.lang.Closure) ]
+		// allows you to access an object within a closure without explicitly referring to it.
 
-        // This is how Java StringBuilders are used:
-        StringBuilder javaStringBuilder = new StringBuilder();
-        javaStringBuilder.append("roses are #FF0000\\n");
-        javaStringBuilder.append("violets are #0000FF\\n");
-        javaStringBuilder.append("all my base\\n")
-        javaStringBuilder.append("are belong to you\\n")
-        String javaResult = javaStringBuilder.toString();
+		// This is how Java StringBuilders are used:
+		StringBuilder javaStringBuilder = new StringBuilder();
+		javaStringBuilder.append("roses are #FF0000\\n");
+		javaStringBuilder.append("violets are #0000FF\\n");
+		javaStringBuilder.append("all my base\\n")
+		javaStringBuilder.append("are belong to you\\n")
+		String javaResult = javaStringBuilder.toString();
 
-        // Groovy-fy the above code, using StringBuilder and with() to get the same result in Groovy
-        String groovyResult
-        // ------------ START EDITING HERE ----------------------
+		// Groovy-fy the above code, using StringBuilder and with() to get the same result in Groovy
+		String groovyResult
+		// ------------ START EDITING HERE ----------------------
+		groovyResult = new StringBuilder().with {
+			append(/roses are #FF0000\n/)
+			append(/violets are #0000FF\n/)
+			append(/all my base\n/)
+			append(/are belong to you\n/)
+			return it;
+		}
+		groovyResult = groovyResult.toString()
+		// ------------ STOP EDITING HERE  ----------------------
+		assert groovyResult == javaResult
+	}
 
+	void test02_CollectMethodOnLists() {
+		// We're often required to iterate through a whole list and and perform some sort of transformation on
+		// some (or all) of the items, returning a new list. Groovy has a method just for that:
+		// http://docs.groovy-lang.org/latest/html/groovy-jdk/java/lang/Object.html#collect(groovy.lang.Closure)
 
-        // ------------ STOP EDITING HERE  ----------------------
-        assert groovyResult == javaResult
-    }
+		// Using collect() and a method from http://docs.groovy-lang.org/latest/html/groovy-jdk/java/util/Collection.html, create
+		// a list of UNIQUE class types from the following list:
+		def differentTypes = [1, 'String', "GString", 'a', 'Another string', 0]
+		def uniqueTypes = []
+		// ------------ START EDITING HERE ----------------------
+		uniqueTypes = differentTypes.collect {
+			it.class
+		}.unique()
+		// ------------ STOP EDITING HERE  ----------------------
+		assert uniqueTypes == [Integer, String]
+	}
 
-    void test02_CollectMethodOnLists() {
-        // We're often required to iterate through a whole list and and perform some sort of transformation on
-        // some (or all) of the items, returning a new list. Groovy has a method just for that:
-        // http://docs.groovy-lang.org/latest/html/groovy-jdk/java/lang/Object.html#collect(groovy.lang.Closure)
+	void test03_FileIteration() {
+		// Groovy's File enhancements includes an iterator that walks through all files
+		// http://docs.groovy-lang.org/latest/html/groovy-jdk/java/io/File.html#eachFileRecurse(groovy.lang.Closure)
 
-        // Using collect() and a method from http://docs.groovy-lang.org/latest/html/groovy-jdk/java/util/Collection.html, create
-        // a list of UNIQUE class types from the following list:
-        def differentTypes = [1, 'String', "GString", 'a', 'Another string', 0]
-        def uniqueTypes = []
-        // ------------ START EDITING HERE ----------------------
+		// Use the eachFileRecurse iterator to find the number of files that contain the string 'Lorem'
+		// under the src directory
+		int count = 0
+		// ------------ START EDITING HERE ----------------------
+		new File('src').eachFileRecurse(FileType.FILES) {
+			if (it.text.contains('Lorem')) {
+				count++;
+			}
+		}
+		// ------------ STOP EDITING HERE  ----------------------
+		assert count == 3
 
+	}
 
-        // ------------ STOP EDITING HERE  ----------------------
-        assert uniqueTypes == [Integer, String]
-    }
+	void test04_ConcludingExercise() {
+		// Using methods from Groovy object (http://docs.groovy-lang.org/latest/html/groovy-jdk/java/lang/Object.html) and
+		// range objects, store all the prime numbers between 200 and 250 in the target variable
+		def primesBetween200And250 = []
+		// ------------ START EDITING HERE ----------------------
+		primesBetween200And250 = (200..250).findAll { int numeroAtual ->
+			(2..<numeroAtual).every { int divisor ->
+				numeroAtual % divisor != 0
+			}
 
-    void test03_FileIteration() {
-        // Groovy's File enhancements includes an iterator that walks through all files
-        // http://docs.groovy-lang.org/latest/html/groovy-jdk/java/io/File.html#eachFileRecurse(groovy.lang.Closure)
+		}
+		// ------------ STOP EDITING HERE  ----------------------
+		assert primesBetween200And250 == [211, 223, 227, 229, 233, 239, 241]
 
-        // Use the eachFileRecurse iterator to find the number of files that contain the string 'Lorem'
-        // under the src directory
-        int count = 0
-        // ------------ START EDITING HERE ----------------------
-
-
-        // ------------ STOP EDITING HERE  ----------------------
-        assert count == 3
-
-    }
-
-    void test04_ConcludingExercise() {
-        // Using methods from Groovy object (http://docs.groovy-lang.org/latest/html/groovy-jdk/java/lang/Object.html) and
-        // range objects, store all the prime numbers between 200 and 250 in the target variable
-        def primesBetween200And250 = []
-        // ------------ START EDITING HERE ----------------------
-
-
-        // ------------ STOP EDITING HERE  ----------------------
-        assert primesBetween200And250 == [211, 223, 227, 229, 233, 239, 241]
-
-    }
+	}
 
 }
